de07633d1469580e23c91007129412c6f69bc30d
anass99t <anass.taher@gmail.com>
2021-04-12 14:10:07 +0000

    start using vue & import fontawsome

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,13 @@
 require('./bootstrap');
+
+import Vue from 'vue'
+import App from './vue/app.vue'
+
+const app = new Vue({
+
+    el:'#app',
+    components:{
+        App
+    }
+
+})
---
+++ resources/js/vue/app.vue
@@ -0,0 +1,38 @@
+<template>
+
+    <div class="todoListContainer">
+        <div class="heading">
+            <h2 class="title" id="title">Todo List</h2>
+            <addItemForm />
+        </div>
+            <listView />
+        
+    </div>
+
+</template>
+
+<script>
+import addItemForm from './components/addItemForm'
+import listView from './components/listView'
+
+export default {
+    components: {
+        addItemForm,
+        listView
+    }
+}
+</script>
+
+<style scoped>
+    .todoListContainer{
+        width: 350px;
+        margin:auto;
+    }
+    .heading{
+        background:#e6e6e6;
+        padding:10px;
+    }
+    .title{
+        text-align:center;
+    }
+</style>
